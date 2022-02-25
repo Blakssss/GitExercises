79bd2fa3e5f8817d1847bfb151586255597e2caf
Mathias <mathias.blak@gmail.com>
2022-02-25 09:23:49 +0100

    added greeting

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,8 +1,13 @@
 package demo;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("It works!");
+    }
+    public static void greeting(){
+        Scanner myObj = new Scanner(System.in);
+        String input = myObj.nextLine();
+        System.out.println("Hej" + input);
     }
 }
